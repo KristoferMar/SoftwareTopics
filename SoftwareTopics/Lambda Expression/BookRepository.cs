e3984287842947c4cb167da3982399a413b32b59
KristoferMar <kristofer@brostu.dk>
2018-11-29 16:16:23 +0100

    Added topic about lambda expressions

---
+++ SoftwareTopics/Lambda Expression/BookRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTopics.Lambda_Expression
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        { 
+            return new List<Book>
+            {
+                new Book() {Title = "Title 1", Price = 5 },
+                new Book() {Title = "Title 2", Price = 7},
+                new Book() {Title = "Title 3", Price = 17}
+            };
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; internal set; }
+        public int Price { get; internal set; }
+    }
+}
---
+++ SoftwareTopics/Lambda Expression/MainProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTopics.Lambda_Expression
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            //This is how we can iterrate through books without using lambda expression
+            var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+
+            //This is how we would do it with Lambda Expression
+            var cheapBooksLambda = books.FindAll(b => b.Price < 10);
+
+            foreach(var book in cheapBooksLambda)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+
+
+            //Same task solved with lambda expression using generic Func
+            Func<int, int> squareLambda = number => number * number;
+            Console.WriteLine(squareLambda(5));
+
+            //One way to area of square without lambda expression.
+            Console.WriteLine(Square(5));
+            Console.ReadLine();
+        }
+
+        //Example method - we can avoid it.
+        static int Square(int number)
+        {
+            return number * number;
+        }
+
+        //Example method - We can avoid it.
+        static bool IsCheaperThan10Dollars(Book book)
+        {
+            return book.Price < 10;
+        }
+    }
+}
