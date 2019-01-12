c6926b8d9d9c501e7da8a9386a7e741d48e638f4
KristoferMar <kristofer@brostu.dk>
2019-01-12 14:20:38 +0100

    Added section about dynamic types.

---
+++ SoftwareTopics/Dynamics/MainProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTopics.Dynamics
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            //This is duable because the type is dynamic.
+            dynamic name = "Kristofer";
+            name = 10;
+
+            //in this case the var will become dynamic in the end.
+            dynamic a = 10;
+            dynamic b = 20;
+            var c = a + b;
+
+            //With the use of dynamics it enables us to ignore casting in many cases.
+        }
+    }
+}
