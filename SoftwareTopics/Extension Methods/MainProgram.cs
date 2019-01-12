e98e06e2b997fc97bee26f07a24ee13ab3ef04d1
KristoferMar <kristofer@brostu.dk>
2019-01-12 10:23:06 +0100

    Created LINQ section.

--- SoftwareTopics/Extension Methods/MainProgram.cs
+++ SoftwareTopics/Extension Methods/MainProgram.cs
@@ -8,13 +8,13 @@ namespace SoftwareTopics.Extension_Methods
 {
     class MainProgram
     {
-        static void Main(string[] args)
-        {
-            string post = "This is a supposed to be a very long blog post blah blah blah...";
-            var shortenedPost = post.Shorten(5);
+        //static void Main(string[] args)
+        //{
+        //    string post = "This is a supposed to be a very long blog post blah blah blah...";
+        //    var shortenedPost = post.Shorten(5);
 
-            Console.WriteLine(shortenedPost);
-            Console.ReadLine();
-        }
+        //    Console.WriteLine(shortenedPost);
+        //    Console.ReadLine();
+        //}
     }
 }
---
+++ SoftwareTopics/LINQ/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTopics.LINQ
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "ADO.NET Step by Step", Price = 5},
+                new Book() {Title = "ASP.NET MVC", Price = 9.99f},
+                new Book() {Title = "ASP.NEt Web API", Price = 12},
+                new Book() {Title = "C# Advanced Topics", Price = 7},
+                new Book() {Title = "C# Advanced Topics", Price = 9}
+            };
+        }
+    }
+}
