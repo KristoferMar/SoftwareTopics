7760e6fc05d3b2204a6b80bcc38aec01d1d339e9
kristofermar <kristofer@brostu.dk>
2019-01-17 04:28:26 +0100

    Given an array of ints, write a C# method to total all the values that are even numbers.

---
+++ SoftwareTopics/Job Interview tasks/IntArrayOnlyEvenNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTopics.Job_Interview_tasks
+{
+    /*
+     * Given an arrat of ints, write a c# method to total all the values that are even numbers.
+     */
+    public class IntArrayOnlyEvenNumbers
+    {
+        //This would be
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine(TotalEvenNumbers(intArray));
+            Console.ReadLine();
+        }
+
+        public static int TotalEvenNumbers(int[] intArray)
+        {
+            var intReturn = intArray.Where(i => i % 2 == 0).Sum();
+
+            return intReturn;
+        }
+    }
+}
